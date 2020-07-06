a1b337a21a4a77e0cf6523412800a153979b84bb
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-06 16:57:49 -0500

    fetch data, set current route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import placeholder from './placeholder.jpg'
 import './App.css';
 
@@ -21,11 +21,12 @@ const RoutesList = (props) => {
 const DateSelection = (props) => {
   return (
     <div>
-      <label for="dates">Plan your trip...</label>
-      <select name='dates' id='dates'>
-        <option value='3-day'>July 11 - July 13</option>
-        <option value='4-day'>July 11 - July 14</option>
-        <option value='5-day'>July 11 - July 15</option>
+      <label htmlFor="dates">Plan your trip...</label>
+      <br/>
+      <select name='dates' id='dates' onChange={props.onChange}>
+        <option value='1'>July 11 - July 13</option>
+        <option value='2'>July 11 - July 14</option>
+        <option value='3'>July 11 - July 15</option>
       </select>
     </div>
   )
@@ -52,29 +53,81 @@ const RouteCampgrounds = (props) => {
   )
 }
 
-const App = () => {
-  const [routesShown, setRoutesShown] = useState('')
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      routes: [],
+      routeInfo: [],
+      campsites: [],
+      availability: [],
+      selectedRoute: 1,
+    }
+    this.fetchRoutes = this.fetchRoutes.bind(this)
+    this.fetchRouteInfo = this.fetchRouteInfo.bind(this)
+    this.fetchCampsites = this.fetchCampsites.bind(this)
+    this.fetchAvailability = this.fetchAvailability.bind(this)
+    this.handleSelectionChange = this.handleSelectionChange.bind(this)
+  }
 
-  // this function would switch which list is displayed under the map (currently selected, or just all of the potential routes)
-  // function setDisplayList(e) {
-  //   setRoutesShown(e.target.value)
-  // }
+  fetchRoutes() {
+    fetch('http://localhost:3001/routes')
+      .then((response) => response.json())
+      .then((json) => { this.setState({ routes: json }) })
+  }
 
+  async fetchRouteInfo() {
+    await fetch('http://localhost:3001/routeinfo')
+      .then((response) => response.json())
+      .then((json) => { this.setState({ routeInfo: json }) })
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Yellowstone National Park Campground Booking Service</h1>
-      </header>
-      <body className="App-body">
-        <h1>Hello there</h1>
-        <DateSelection />
-        <Map />
-        <h2>Book your trip now!</h2>
-        <RoutesList />
-      </body>
-    </div>
-  );
+  async fetchCampsites() {
+    await fetch('http://localhost:3001/campsites')
+      .then((response) => response.json())
+      .then((json) => { this.setState({ campsites: json }) })
+  }
+
+  async fetchAvailability() {
+    await fetch('http://localhost:3001/availability')
+      .then((response) => response.json())
+      .then((json) => { this.setState({ availability: json }) })
+  }
+
+  handleSelectionChange(e) {
+    this.setState({selectedRoute: parseInt(e.target.value)})
+  }
+  
+
+  render() {
+    if (this.state.routes.length === 0) {
+      this.fetchRoutes()
+    }
+    if (this.state.routeInfo.length === 0) {
+      this.fetchRouteInfo()
+    }
+    if (this.state.campsites.length === 0) {
+      this.fetchCampsites()
+    }
+    if (this.state.availability.length === 0) {
+      this.fetchAvailability()
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Yellowstone National Park Campground Booking Service</h1>
+        </header>
+        <div className="App-body">
+          <h1>Hello there</h1>
+          <DateSelection selectedRoute={this.state.selectedRoute} onChange={this.handleSelectionChange} />
+          <Map selectedRoute={this.state.selectedRoute} />
+          <h2>Book your trip now!</h2>
+          <RoutesList selectedRoute={this.state.selectedRoute} routes={this.state.routes} routeInfo={this.state.routeInfo} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
