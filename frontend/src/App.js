e788f77ebb678eacc022d51ad789d41504f73072
Alexis <alexisdkreager@gmail.com>
2020-07-06 11:21:27 -0500

    setting up front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import placeholder from './placeholder.jpg'
 import './App.css';
 
+//Map of Yellowstone, hoping to apply map html and display routes on map
 const Map = () => {
-
+  return (
+    <img src={placeholder} alt="Map of Yellowstone National Park" />
+  )
 }
 
-const RouteList = () => {
+//Displays all of the potential routes you could take, and the stops you could make
+const RoutesList = () => {
+  return (
+    <ul>
 
+    </ul>
+  )
 }
 
+const DateSelection = () => {
+  return (
+    <div>
+      <label for="dates">Plan your trip...</label>
+      <select name='dates' id='dates'>
+        <option value='3-day'>July 11 - July 13</option>
+        <option value='4-day'>July 11 - July 14</option>
+        <option value='5-day'>July 11 - July 15</option>
+      </select>
+    </div>
+  )
+}
+
+//Displays one campground list item
+const Campground = () => {
+  return (
+    <li>
+      <button type='button'>Book now!</button>
+      {/* Campground name
+      Campground description
+      Available? */}
+    </li>
+  )
+}
+
+//Displays all of the campground list items contained in a single route
+const RouteCampgrounds = () => {
+  return (
+    <ul>
+      {/* ALL THE CAMPGROUNDS IN THAT ROUTE WILL BE IN THIS LIST */}
+    </ul>
+  )
+}
 
 const App = () => {
+  const [routesShown, setRoutesShown] = useState('')
+
+  function setDisplayList(e) {
+    setRoutesShown(e.target.value)
+  }
 
 
   return (
@@ -21,8 +67,10 @@ const App = () => {
       </header>
       <body className="App-body">
         <h1>Hello there</h1>
-        {/* <Map />
-        <RouteList /> */}
+        <DateSelection />
+        <Map />
+        <h2>Book your trip now!</h2>
+        <RoutesList />
       </body>
     </div>
   );
