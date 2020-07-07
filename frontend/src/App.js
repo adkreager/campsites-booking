8c40b3804d4d654c3dbae6f1998d30b76ebe82bd
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-07 10:19:09 -0500

    working on displaying info in front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import placeholder from './placeholder.jpg'
+import yellowstone from './yellowstone-map.png'
 import './App.css';
 
-//Map of Yellowstone, hoping to apply map html and display routes on map
-const Map = (props) => {
-  return (
-    <img src={placeholder} alt="Map of Yellowstone National Park" />
-  )
-}
 
-//Displays all of the potential routes you could take, and the stops you could make
-const RoutesList = (props) => {
-  return (
-    <ul>
-      {props.routes.map(route => <li>{route.routename}</li>)}
-    </ul>
-  )
-}
-
+//Dates available to choose from
 const DateSelection = (props) => {
   return (
     <div>
@@ -33,10 +19,39 @@ const DateSelection = (props) => {
   )
 }
 
+//Map of Yellowstone, hoping to apply map html and display routes on map
+const Map = (props) => {
+  return (
+    <img src={yellowstone} alt="Map of Yellowstone National Park" />
+  )
+}
+
+//Displays all of the potential routes you could take, and the stops you could make
+const RoutesList = (props) => {
+
+  function checkIfRenderable() {
+    if (props.selectedRoute !== undefined && props.routeInfo !== undefined) {
+      return <RouteCampgrounds selectedRoute={props.selectedRoute} routeInfo={props.selectedRouteInfo} />
+    }
+  }
+  return (
+    <div>
+      <ul>
+        {props.routes.map(route => <li>{route.routename}</li>)}
+      </ul>
+      {/* <RouteCampgrounds selectedRoute={props.selectedRoute} routeInfo={props.selectedRouteInfo} /> */}
+      {/* {checkIfRenderable()} */}
+    </div>
+  )
+}
+
 //Displays all of the campground list items contained in a single route
 const RouteCampgrounds = (props) => {
+
   return (
     <ul>
+      {/* <li>{selectedRoute.routename}</li>
+      {props.routeInfo.map(site => <li>{routeInfo.description}</li>)} */}
     </ul>
   )
 }
@@ -56,6 +71,7 @@ class App extends React.Component {
     this.fetchAvailability = this.fetchAvailability.bind(this)
     this.handleSelectionChange = this.handleSelectionChange.bind(this)
     this.fetchSpecificRoute = this.fetchSpecificRoute.bind(this)
+    //CHANGE BOOL FROM FALSE TO TRUE IF BOOKED
     this.postBookedStatus = this.postBookedStatus.bind(this)
   }
 
@@ -86,7 +102,7 @@ class App extends React.Component {
   async postBookedStatus(campsiteid) {
     await fetch(``)
       .then((response) => response.json())
-      .then((json) => {this.setState({availability: json})})
+      .then((json) => { this.setState({ availability: json }) })
   }
 
   handleSelectionChange(e) {
@@ -94,10 +110,6 @@ class App extends React.Component {
     this.setState({ selectedRoute: this.state.routes[id - 1] }, () => {
       this.fetchSpecificRoute(id)
     })
-  }
-
-  displaySelectedRoute() {
-    
   }
 
   render() {
@@ -122,8 +134,9 @@ class App extends React.Component {
           <DateSelection selectedRoute={this.state.selectedRoute} onChange={this.handleSelectionChange} />
           <Map selectedRoute={this.state.selectedRoute} />
           <h2>Book your trip now!</h2>
-          <RoutesList routes={this.state.routes} onClick={this.displaySelectedRoute}/>
-          <RouteCampgrounds selectedRoute={this.state.selectedRoute} routeInfo={this.state.selectedRouteInfo} />
+          <React.Fragment>
+            <RoutesList routes={this.state.routes} selectedRoute={this.state.selectedRoute} routeInfo={this.state.selectedRouteInfo} />
+          </React.Fragment>
         </div>
       </div>
     )
