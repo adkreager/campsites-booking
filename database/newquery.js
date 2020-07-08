a0c39dc06ad59bd6979d0c262d4bd005af8e0ce7
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-08 14:13:00 -0500

    update getLodgingsInfo

--- database/newquery.js
+++ database/newquery.js
@@ -20,7 +20,21 @@ const getRoutes = (request, response) => {
 }
 
 //GET LIST OF LODGINGS for a specific route
+const getLodgingsInfo = (request, response) => {
+    let id = request.params.id
+    let day = request.params.day
+    let type = request.params.type
+
+    //gets all of the lodgings for a route on a day number and of a certain type
+    connection.query('SELECT * FROM t_lodgings WHERE routeid=$1 AND daynumber=$2 AND lodgingtype=$3', [id, day, type], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
 
 module.exports = {
     getRoutes,
+    getLodgingsInfo
 }
