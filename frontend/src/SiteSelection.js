560885e2bdc22bf19768a67fc226afaf7f308828
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-08 13:26:36 -0500

    boilerplate for site selection

---
+++ frontend/src/SiteSelection.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SiteSelection = (props) => {
+    return (
+      <div id="routeSelection">
+      <label htmlFor="route">Site...</label><br />
+        <select name='route' id='route' onChange={props.onChange}>
+          <option disabled selected hidden></option>
+
+            DROP DOWN LIST POPULATE WITH MAP HERE
+
+          <option value='1'>Route 1 (2 stops)</option>
+          <option value='2'>Route 2 (3 stops)</option>
+          <option value='3'>Route 3 (4 stops)</option>
+        </select>
+      </div>
+    )
+  }
+
+export default SiteSelection
