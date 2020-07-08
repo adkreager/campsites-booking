b26f1c84afbc1601af845f49c4a657ca107438a4
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-08 13:25:47 -0500

    Update DateSelection.js

--- frontend/src/DateSelection.js
+++ frontend/src/DateSelection.js
@@ -3,14 +3,15 @@ import React from 'react';
 //Dates available to choose from
 const DateSelection = (props) => {
     return (
-      <div>
-        <label htmlFor="dates">Plan your trip...</label>
-        <br />
+      <div id="dateSelection">
+        <label htmlFor="dates">Start date...</label>
         <select name='dates' id='dates' onChange={props.onChange}>
           <option disabled selected hidden></option>
-          <option value='1'>July 11 - July 13</option>
-          <option value='2'>July 11 - July 14</option>
-          <option value='3'>July 11 - July 15</option>
+          <option value='2020-07-11'>July 11</option>
+          <option value='2020-07-12'>July 12</option>
+          <option value='2020-07-13'>July 13</option>
+          <option value='2020-07-14'>July 14</option>
+          <option value='2020-07-15'>July 15</option>
         </select>
       </div>
     )
