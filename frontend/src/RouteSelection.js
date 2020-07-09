d922dfbe01019cf4e2f26f9242a7da651fdc815d
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-09 14:41:33 -0500

    Update RouteSelection.js

--- frontend/src/RouteSelection.js
+++ frontend/src/RouteSelection.js
@@ -5,7 +5,7 @@ const RouteSelection = (props) => {
   return (
     <div id="routeSelection">
       <label htmlFor="route">Route...</label>
-      <select name='route' id='route' onChange={props.onChange}>
+      <select name='route' id='route' onChange={props.onRouteChange}>
         <option disabled selected hidden></option>
         {props.routes.map(route => <option value={route.routeid}>{route.routename} ({route.routedesc})</option>)}
       </select>
