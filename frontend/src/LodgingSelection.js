d09394d131e27989518ae379c69ba044333e261c
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-09 16:05:25 -0500

    lodging options drop down list

--- frontend/src/LodgingSelection.js
+++ frontend/src/LodgingSelection.js
@@ -1,17 +1,13 @@
 import React from 'react'
 
 const LodgingSelection = (props) => {
-  props.fetchLodgings(props.selectedRoute.routeid, props.currentDay, )
+  props.fetchLodgings(props.selectedRoute.routeid, props.currentDay, props.lodgingType)
     return (
       <div>
       <label htmlFor="lodging">Lodging...</label> <br />
         <select name='lodging' id='lodging' onChange={props.onChange}>
           <option disabled selected hidden></option>
           {props.lodging.map(lodge => <option value={lodge.lodgingid}>{lodge.lodgingname}</option>)}
-
-          <option value='1'>Route 1 (2 stops)</option>
-          <option value='2'>Route 2 (3 stops)</option>
-          <option value='3'>Route 3 (4 stops)</option>
         </select>
       </div>
     )
