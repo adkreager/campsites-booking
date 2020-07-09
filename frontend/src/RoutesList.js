1994086a4817eaa63f8b6a27f9a9043599f32c19
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-09 09:56:43 -0500

    get correct number of cards for booking

--- frontend/src/RoutesList.js
+++ frontend/src/RoutesList.js
@@ -3,10 +3,18 @@ import RouteCampgrounds from './RouteCampgrounds'
 
 //Displays all of the potential routes you could take
 const RoutesList = (props) => {
+    let arr = [];
+    if (props.selectedRoute !== undefined) {
+        for (let i = 1; i <= props.selectedRoute.numberofdays; i++) {
+            arr.push(i)
+        }
+    }
+    let cards = arr.map(day => <RouteCampgrounds currentDay={day} selectedRoute={props.selectedRoute} onOKClick={props.onOKClick}/>);
+
     return (
         <div id="card-container">
-            <RouteCampgrounds onRadio={props.onRadio} />
-           <a href='#' className="btn btn-primary">Book Now!!!</a>
+            {cards}
+            <a href='#' className="btn btn-primary" >Book Now!!!</a>
         </div>
     )
 }
