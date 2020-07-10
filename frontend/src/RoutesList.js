a8830711a430dcf453f21f34b8cadf939f2d7875
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-09 23:30:35 -0500

    PUT

--- frontend/src/RoutesList.js
+++ frontend/src/RoutesList.js
@@ -2,26 +2,48 @@ import React from 'react'
 import RouteCampgrounds from './RouteCampgrounds'
 
 //Displays all of the potential routes you could take
-const RoutesList = (props) => {
-    let arr = [];
-    if (props.selectedRoute !== undefined) {
-        for (let i = 1; i <= props.selectedRoute.numberofdays; i++) {
-            arr.push(i)
-        }
+class RoutesList extends React.Component {
+    constructor(props) {
+        super(props)
     }
-    let cards = arr.map(day => <RouteCampgrounds currentDay={day} selectedRoute={props.selectedRoute} onOKClick={props.onOKClick} date={props.date}/>);
 
-    return (
-        <div className="flex">
+    async postBookedStatus(lodgingid) {
+        let data = {
+          id: lodgingid,
+        }
+    
+        await fetch(`http://localhost:3001/book/${data.id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        }).then(response => response.json())
+          .then(data => {
+            console.log('Success:', data);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
+      }
+
+    render() {
+        let arr = [];
+        if (this.props.selectedRoute !== undefined) {
+            for (let i = 1; i <= this.props.selectedRoute.numberofdays; i++) {
+                arr.push(i)
+            }
+        }
+        let cards = arr.map(day => <RouteCampgrounds post={this.postBookedStatus} currentDay={day} selectedRoute={this.props.selectedRoute} setTotal={this.setTripTotal}/>);
+
+        return (
             <div className="flex">
-                {cards}
+                <div className="flex">
+                    {cards}
+                </div>
             </div>
-            <div>
-                <a href='#' className="btn btn-primary" >Book Now!!!</a>
-                <p>Summary Info here <br /> price <br /> etc.</p>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default RoutesList
